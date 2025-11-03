7e209c56a53a9c1473b19afd278f9e4e0c6ff8b0
Alejandro <arde862@gmail.com>
2025-11-03 00:29:36 -0300

    Terminadas las actividades 1 y 2

---
+++ tp2-react/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Counter } from "./components/Counter.jsx";
+import { Login } from "./components/login.jsx";
+import { App } from "./pages/App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
