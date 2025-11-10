afc37d758f74feec4c5b5c336bb6277ac2ed50ac
Alejandro <BB_rae@hotmail.com>
2025-11-10 16:34:30 -0300

    terminado el tp3

---
+++ tp3-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { CharacterInfo } from "./components/CharacterInfo";
+import { Loading } from "./components/Loading";
+import { useFetch } from "./hooks/useFetch";
+import { useCounter } from "./hooks/useCounter";
+
+export const App = () => {
+  const { count, handleIncrement, handleDecrement } = useCounter(1);
+  const { data, isLoading } = useFetch(
+    `https://dragonball-api.com/api/characters/${count}`
+  );
+
+  return (
+    <>
+      <h1>Personajes de Dragon Ball</h1>
+      {isLoading && <Loading />}
+      {data && !isLoading && <CharacterInfo character={data} />}
+      <div>
+        <button onClick={handleDecrement}>Anterior</button>
+
+        <button onClick={handleIncrement}>Siguiente</button>
+      </div>
+    </>
+  );
+};
